02c08578d3ee130fb075e82b3cebaa9482d811a4
Samuel Marks <samuelmarks@gmail.com>
2018-05-21 19:30:04 +1000

    Implemented getNewAccessToken

--- api/email/ms_graph_api.ts
+++ api/email/ms_graph_api.ts
@@ -3,13 +3,11 @@ import { IConfig } from '../config/models.d';
 import { IMail } from './ms_graph_api.d';
 
 export class MSGraphAPI {
+    public client_secret?: string;
+
     protected static _instance: MSGraphAPI;
     public tenant_id?: string;
     public client_id?: string;
-    public refresh_token?: string;
-    public access_token?: string;
-
-    constructor() {}
 
     public static instance(config?: IConfig) {
         if (this._instance == null)
@@ -23,8 +21,35 @@ export class MSGraphAPI {
 
         return this._instance;
     }
+    public refresh_token?: string;
+    public access_token?: string;
 
-    // TODO: Implement refresh_token->access_token flow
+    public getNewAccessToken(callback: (error: Error, mail?: IMail) => void): any {
+        // https://github.com/microsoftgraph/microsoft-graph-docs/blob/master/concepts/auth_v2_user.md
+        // #5-use-the-refresh-token-to-get-a-new-access-token
+
+        const body = JSON.stringify({
+            client_id: this.client_id,
+            client_secret: this.client_secret,
+            refresh_token: this.refresh_token,
+            grant_type: 'refresh_token',
+            scope: 'mail.send offline_access',
+            redirect_uri: 'https://glaucoma.org.au/admin/email'
+        });
+
+        httpRequest<IMail>({
+                method: 'POST',
+                host: 'https://login.microsoftonline.com',
+                path: '/common/oauth2/v2.0/token',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Content-Length': Buffer.byteLength(body)
+                }
+            }, body
+        )
+            .then(token_response => callback(void 0, token_response))
+            .catch(callback);
+    }
 
     public sendEmail(mail: IMail, callback: (error: Error, mail?: IMail) => void): void {
         console.info('sendEmail::mail:', mail, ';');
--- api/email/route.ts
+++ api/email/route.ts
@@ -5,12 +5,14 @@ import { fmtError } from 'custom-restify-errors';
 import { Query } from 'waterline';
 import * as querystring from 'querystring';
 import { has_body, mk_valid_body_mw } from 'restify-validators';
+import tryEach from 'async/tryEach';
 
 import { httpRequest } from '../../test/SampleData';
 import { has_auth } from '../auth/middleware';
 import { getConfig, upsertConfig } from '../config/sdk';
 import { readManyTemplates } from '../template/sdk';
 import { IRiskRes } from '../risk_res/models.d';
+import { IConfig } from '../config/models.d';
 import { IMail, ITokenResponse } from './ms_graph_api.d';
 import { MSGraphAPI } from './ms_graph_api';
 
@@ -102,40 +104,59 @@ export const sendEmail = (app: restify.Server, namespace: string = ''): void => 
     app.post(`${namespace}/:recipient/:risk_id`,
         (req: restify.Request & IOrmReq, res: restify.Response, next: restify.Next) => {
             waterfall([
-                    cb => getConfig(req, cb),
-                    (config, cb) => readManyTemplates(req, (err, templates) =>
-                        cb(err, config, templates)
-                    ),
-                    (config, templates, cb) => {
-                        const RiskRes: Query = req.getOrm().waterline.collections['risk_res_tbl'];
-                        RiskRes.findOne({ id: req.params.risk_id }).exec((err, risk_res) =>
-                            cb(err, risk_res, config, templates));
-                    },
-                    (risk_res: IRiskRes, config, templates, cb) => {
-                        const templatesMap = new Map<string, string>();
-                        templates.templates.forEach(template =>
+                cb => getConfig(req, cb),
+                (config, cb) => readManyTemplates(req, (err, templates) =>
+                    cb(err, config, templates)
+                ),
+                (config, templates, cb) => {
+                    const RiskRes: Query = req.getOrm().waterline.collections['risk_res_tbl'];
+                    RiskRes
+                        .findOne({ id: req.params.risk_id })
+                        .exec((err, risk_res) => cb(err, risk_res, config, templates));
+                },
+                (risk_res: IRiskRes, config: IConfig, templates, cb) => {
+                    const templatesMap = new Map<string, string>();
+                    templates.templates
+                        .forEach(template =>
                             templatesMap.set(template.kind, template.contents)
                         );
 
-                        const safeGet = (map: Map<any, any>, key: any) =>
-                            map.has(key) ? map.get(key) : void 0;
+                    const safeGet = (map: Map<any, any>, key: any) =>
+                        map.has(key) ? map.get(key) : void 0;
 
-                        const share_url = `https://calculator.glaucoma.org.au/${risk_res.id}`;
+                    const share_url = `https://calculator.glaucoma.org.au/${risk_res.id}`;
 
-                        MSGraphAPI.instance(config).sendEmail(
-                            {
-                                recipient: req.params.recipient,
-                                subject: safeGet(templatesMap, 'email_subject'),
-                                content: `${safeGet(templatesMap, 'email_contents')} ${share_url}`
-                            }, cb
-                        );
-                    }
-                ],
-                (errors, mail: IMail) => {
-                    if (errors != null) return next(fmtError(errors));
-                    res.json(mail);
-                    return next();
-                });
+                    return cb(void 0, config, {
+                        recipient: req.params.recipient,
+                        subject: safeGet(templatesMap, 'email_subject'),
+                        content: `${safeGet(templatesMap, 'email_contents')} ${share_url}`
+                    });
+                },
+                (config: IConfig, mail: IMail, cb) =>
+                    tryEach([
+                        c_b => MSGraphAPI
+                            .instance(config)
+                            .sendEmail(mail, c_b),
+                        c_b => MSGraphAPI
+                            .instance(config)
+                            .getNewAccessToken((e, tokens) => {
+                                if (e != null) return c_b(e);
+                                req.body = tokens;
+                                upsertConfig(req, (er, re) => {
+                                    if (er != null) return c_b(er);
+                                    config = re;
+                                    return c_b(void 0, re);
+                                });
+                            }),
+                        c_b => MSGraphAPI
+                            .instance(config)
+                            .sendEmail(mail, c_b),
+                    ], cb)
+            ], (errors, mail: IMail) => {
+                if (errors != null) return next(fmtError(errors));
+                res.json(mail);
+                return next();
+            });
         }
     );
 };
