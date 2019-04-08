7fcb203d65c8fc1faff417e9474cf5d9f1a254fa
Samuel Marks <samuelmarks@gmail.com>
2019-04-08 13:56:51 +1000

    [package.json,package-lock.json,typings.json] Added simple-statistics (Postgres without Apache); updated nodejs-utils; [api/analytics/routes.ts] Expose various stats on client risk and age using simple-statistics

--- api/analytics/routes.ts
+++ api/analytics/routes.ts
@@ -1,11 +1,14 @@
+import { parallel } from 'async';
 import * as restify from 'restify';
 import { Query, WLError } from 'waterline';
 import { fmtError, NotFoundError } from 'custom-restify-errors';
 import { has_body, mk_valid_body_mw_ignore } from 'restify-validators';
 import { JsonSchema } from 'tv4';
+import { resolveIntFromObject } from 'nodejs-utils'
 import { IOrmReq } from 'orm-mw';
+import * as simpleStatistics from 'simple-statistics';
+
 import { IRiskRes } from '../risk_res/models.d';
-import { parallel } from 'async';
 
 /* tslint:disable:no-var-requires */
 const risk_res_schema: JsonSchema = require('./../../test/api/risk_res/schema');
@@ -25,22 +28,56 @@ export const create = (app: restify.Server, namespace: string = ''): void => {
     );
 };
 
+// const funcs = Object.freeze(['average', 'mean', 'max', 'min']);
+
+
+const funcs = Object.freeze([
+    'min',
+    'max',
+    'sum',
+
+    'mean',
+    'mode',
+    'median',
+    'harmonicMean',
+    'geometricMean',
+    'rootMeanSquare',
+    'sampleSkewness',
+
+    'variance',
+    'sampleVariance',
+    'standardDeviation',
+    'sampleStandardDeviation',
+    'medianAbsoluteDeviation',
+    'interquartileRange'
+]);
+
+const f = (func: readonly string[], obj: {}): {} =>
+    funcs
+        .map(func => ({ [func]: simpleStatistics[func](obj) }))
+        .reduce((a, b) => Object.assign(a, b), {});
+
 export const getAll = (app: restify.Server, namespace: string = ''): void => {
     app.get(namespace,//has_auth('admin'),
         (req: restify.Request & IOrmReq, res: restify.Response, next: restify.Next) => {
             const RiskRes: Query = req.getOrm().waterline.collections['risk_res_tbl0'];
 
-            const [condition, valuesToEscape] = ((): [string, string[]] => {
+            const [condition, where_condition, valuesToEscape] = ((): [string, string, string[]] => {
                 if (req.query == null || req.query.startDatetime == null || req.query.endDatetime == null)
-                    return ['', []];
+                    return ['', '', []];
 
                 req.query.startDatetime = decodeURIComponent(req.query.startDatetime);
                 req.query.endDatetime = decodeURIComponent(req.query.endDatetime);
-                return [`"createdAt" >= $1 AND "updatedAt" <= $2`, [req.query.startDatetime, req.query.endDatetime]];
+
+                const r: [string, string, string[]] = [
+                    `"createdAt" >= $1 AND "updatedAt" <= $2`, '', [req.query.startDatetime, req.query.endDatetime]
+                ];
+                r[1] = `WHERE ${r[0]}`;
+                return r;
             })();
 
             parallel({
-                risk_res:
+                row_wise_stats:
                     callb => RiskRes
                         .find(condition ? {
                             where: {
@@ -51,13 +88,24 @@ export const getAll = (app: restify.Server, namespace: string = ''): void => {
                         .exec((error: WLError | Error, risk_res: IRiskRes[]) => {
                             if (error != null) return callb(error as Error);
                             else if (risk_res == null || !risk_res.length) return next(new NotFoundError('RiskRes'));
-                            return callb(void 0, risk_res);
+
+                            return callb(void 0,
+                                Object.assign(
+                                    // Object.keys(risk_res[0])
+                                    {
+                                        column: ['age', 'client_risk']
+                                            .map(col => ({ [col]: f(funcs, risk_res.map(rr => rr[col])) }))
+                                            .reduce((prev, curr) => Object.assign(prev, curr), {})
+                                    },
+                                    { risk_res }
+                                )
+                            );
                         }),
                 ethnicity_agg:
                     callb => RiskRes.query(
                         `SELECT ethnicity, COUNT(*)
                         FROM risk_res_tbl0
-                        ${condition ? 'WHERE ' + condition : ''} 
+                        ${where_condition} 
                         GROUP BY ethnicity ;`,
                         valuesToEscape, (e, r) => {
                             if (e != null) return next(fmtError(e));
@@ -73,14 +121,10 @@ export const getAll = (app: restify.Server, namespace: string = ''): void => {
                         COUNT(client_risk) filter (WHERE client_risk > 50 AND client_risk <= 75) AS high,
                         COUNT(client_risk) filter (WHERE client_risk > 75) AS greatest
                     FROM risk_res_tbl0
-                    ${condition ? 'WHERE ' + condition : ''} ;`, valuesToEscape, (e, r) => {
+                    ${where_condition} ;`, valuesToEscape, (e, r) => {
                         if (e != null) return next(fmtError(e));
                         else if (r.rows == null || !r.rows.length) return next(new NotFoundError('RiskRes'));
-                        return callb(void 0, r.rows.map(row =>
-                            Object.keys(row)
-                                .map(k => ({ [k]: isNaN(row[k]) ? row[k] : parseInt(row[k]) }))
-                                .reduce((a, b) => Object.assign(a, b), {})
-                         )[0]);
+                        return callb(void 0, r.rows.map(resolveIntFromObject)[0]);
                     })
             }, (err, results) => {
                 if (err != null) return next(fmtError(err));
